6a3d9fde9b78b17718054c12f0e4daf807974e86
ZHUQI XIANG <zhuqixiang720@gmail.com>
2025-05-13 09:24:44 +0800

    Update the settings to make the app work

--- config/api.js
+++ config/api.js
@@ -1,4 +1,5 @@
-// const ApiRoot = 'http://localhost:8360';
+const ApiRoot = 'http://localhost:8360';
+// const ApiRoot = 'http://www.hiolabs.com:8360';
 // const ApiRoot = 'http://192.168.0.113:8360';
 const ApiRootUrl = ApiRoot + '/api/'
 
